0a701968fb2ffa1b875609d934618ee3017b71a6
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-05-29 10:04:35 -0600

    basic starter ui

---
+++ user_interface/app.py
@@ -0,0 +1,20 @@
+from operator import index
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    return render_template('upload.html')
+
+@app.route('/live', methods=['GET'])
+def live():
+    return render_template('live.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
