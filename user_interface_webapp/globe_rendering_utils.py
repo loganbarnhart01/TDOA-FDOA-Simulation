ede90103dd9870047b92235d081a2b8a561e5e3c
Kelly Fisher <kellyjane1314@gmail.com>
2024-05-30 15:59:46 -0600

    globe update

--- user_interface_webapp/globe_rendering_utils.py
+++ user_interface_webapp/globe_rendering_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from plotly import graph_objects as go
+import csv
 
 def render_plot(data):
     """
@@ -13,10 +14,36 @@ def render_plot(data):
     returns:
         html for plotly globe with template
     """
+
+    file_path = "flight_data/iata-icao-lat-lon.csv"
+    airport_data = parse_icao_lat_lon(file_path)
+
+    # Unpack data from airport_data list
+    latitudes, longitudes, icaos, iatas, airport_names = zip(*airport_data)
+
+    # Create text for each data point containing all information
+    texts = [f"ICAO: {icao}<br>IATA: {iata}<br>Airport Name: {name}<br>Latitude: {lat}<br>Longitude: {lon}" 
+            for lat, lon, icao, iata, name in zip(latitudes, longitudes, icaos, iatas, airport_names)]
+
+    fig = go.Figure()
+
+    # Scattergeo for airport data
+    fig.add_trace(go.Scattergeo(
+        lat=latitudes,
+        lon=longitudes,
+        text=texts,
+        mode='markers',
+        marker=dict(
+            symbol='hash-open',
+            color='#47476c',
+            size=10
+        )
+    ))
+
     latitude = data['lat']
     longitude = data['lon']
 
-    fig = go.Figure()
+    # fig = go.Figure()
 
     #Line customization    
     marker = dict(
@@ -68,7 +95,8 @@ def render_plot(data):
                           lakecolor='#cdd6f4',
                           showcountries=True,
                           countrycolor='#cdd6f4',
-                          bgcolor='#1e1e2e'
+                          bgcolor='#1e1e2e',
+                        #   showstates=True
                         ),
                       showlegend=False
                       )
@@ -116,3 +144,13 @@ def triangle_orientation(bearing):
     else:
         return '-up' # default
     
+def parse_icao_lat_lon(file_path):
+    airport_data = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header row in CSV file
+        for row in reader:
+            country_code, region_name, iata, icao, airport, latitude, longitude = row
+            # append latitude, longitude, ICAO, and IATA to airport_data list
+            airport_data.append((float(latitude), float(longitude), icao, iata, airport))
+    return airport_data
