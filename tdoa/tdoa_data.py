b2859149b155171d5e24e4c6313b6f7fd0365559
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-06-04 15:51:13 -0600

    renaming for later

--- tdoa/tdoa_data.py
+++ tdoa/tdoa_data.py
@@ -6,7 +6,28 @@ def main():
     receivers = [(0,1,0), (1,0,0), (1,1,0)]
     tdoa = generate_true_2d_tdoa_data(emitter, receivers)
     print(tdoa)
+
+def generate_tdoa_data(emitter, receivers, mode=''):
     
+    if mode == 'fake_signals':
+        return
+
+    if mode == 'random_emitter':
+        return
+    
+    if mode == 'fixed_emitter':
+        if emitter == None:
+            raise ValueError('Emitter location must be provided')
+        
+        if len(receivers) == 3:
+            return generate_true_2d_tdoa_data(emitter, receivers)
+        if len(receivers) >= 4:
+            return generate_true_3d_tdoa_data(emitter, receivers)
+
+
+def generate_random_2d_tdoa_data(receivers):
+    return
+
 def generate_true_2d_tdoa_data(emitter, receivers):
     '''
         Generates precide time difference of arrival provided with emitter and receiver locations
