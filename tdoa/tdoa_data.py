4071eabd2a73ef5e8f9e821c1055a008cd7a334b
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-06-04 16:16:22 -0600

    added random tdoa data option

--- tdoa/tdoa_data.py
+++ tdoa/tdoa_data.py
@@ -1,23 +1,26 @@
 import numpy as np
 from geopy import distance
 
+c = 299792458.0 # speed of light in m/s
+
+
 def main():
     emitter = (1,1,1000)
     receivers = [(0,1,0), (1,0,0), (1,1,0)]
     tdoa = generate_true_2d_tdoa_data(emitter, receivers)
     print(tdoa)
 
-def generate_tdoa_data(emitter, receivers, mode=''):
+def generate_tdoa_data(emitter, receivers, mode='random_emitter'):
     
     if mode == 'fake_signals':
         return
 
     if mode == 'random_emitter':
-        return
+        return generate_random_tdoa_data(receivers)
     
     if mode == 'fixed_emitter':
         if emitter == None:
-            raise ValueError('Emitter location must be provided')
+            raise ValueError('Emitter location must be provided for mode = \'fixed_emitter\'')
         
         if len(receivers) == 3:
             return generate_true_2d_tdoa_data(emitter, receivers)
@@ -25,21 +28,36 @@ def generate_tdoa_data(emitter, receivers, mode=''):
             return generate_true_3d_tdoa_data(emitter, receivers)
 
 
-def generate_random_2d_tdoa_data(receivers):
-    return
+def generate_random_tdoa_data(receivers):
+    n = len(receivers)
+
+    tdoa = np.zeros( (n, n) )
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            distance = distance.distance(receivers[i], receivers[j]).m
+            max_time_diff = distance/c
+            tdoa[i,j] = np.random.random() * max_time_diff
+            tdoa[j,i] = tdoa[i,j]
+
+    return tdoa
+
 
 def generate_true_2d_tdoa_data(emitter, receivers):
     '''
         Generates precide time difference of arrival provided with emitter and receiver locations
         Does not consider altitude - hence 2d data
 
-        emitter: (lat, lon, alt) 
-        receivers: [(lat, lon, alt), (lat, lon, alt), ...] 
-        returns: [tdoa1, tdoa2, ...], tdoai == 0 is the reciever which first receives the signal
+        emitter: (lat, lon), position for emitter if known
+        receivers: [(lat, lon), (lat, lon), ...] = coords for [receiver0, receiver1, ...]
+        returns: matrix of tdoa values with shape (num_receivers, num_receivers)  
+                 tdoa[i,j] == tdoa[j,i] is the time difference between receiver i and j  
     '''
-    
 
-    c = 299792458.0 # speed of light in m/s
+    if emitter:
+        assert len(emitter) == 2 
+    for r in receivers:
+        assert len(r) == 2  
 
     emitter_lat, emitter_lon, _ = emitter
 
@@ -52,7 +70,19 @@ def generate_true_2d_tdoa_data(emitter, receivers):
 
     times = np.array(distances)/c
     min_time = min(times)
-    tdoa = times - min_time
+    arrival_times = times - min_time
+    n = len(arrival_times)
+    
+    tdoa = np.zeros( (n, n))
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if i == j:
+                continue
+            diff = np.abs(arrival_times[i] - arrival_times[j])
+            tdoa[i,j] = diff
+            tdoa[j,i] = diff
+    
     return tdoa
 
 def generate_true_3d_tdoa_data(emitter, receivers):
@@ -60,11 +90,15 @@ def generate_true_3d_tdoa_data(emitter, receivers):
         Generates precide time difference of arrival provided with emitter and receiver locations
         Does consider altitude - hence 3d data
 
-        emitter: (lat, lon, alt) 
-        receivers: [(lat, lon, alt), (lat, lon, alt), ...] 
-        returns: [tdoa1, tdoa2, ...], tdoai == 0 is the reciever which first receives the signal
+        emitter: (lat, lon, alt), position for emitter if known
+        receivers: [(lat, lon, alt), (lat, lon, alt), ...], positions for [receiver0, receiver1, ...]
+        returns: (num_receivers, num_receivers) matrix of tdoa values. 
+                 tdoa[i,j] == tdoa[j,i] is the time difference between receiver i and j  
     '''
-    c = 299792458.0 # speed of light in m/s
+    if emitter:
+        assert len(emitter) == 2 
+    for r in receivers:
+        assert len(r) == 2  
     
     emitter_lat, emitter_lon, emitter_alt = emitter
 
@@ -82,8 +116,19 @@ def generate_true_3d_tdoa_data(emitter, receivers):
 
     # time differences in seconds
     min_time = min(times)
-    tdoa = times - min_time
+    arrival_times = times - min_time
+    n = len(arrival_times)
+    
+    tdoa = np.zeros( (n, n))
 
+    for i in range(n):
+        for j in range(i + 1, n):
+            if i == j:
+                continue
+            diff = np.abs(arrival_times[i] - arrival_times[j])
+            tdoa[i,j] = diff
+            tdoa[j,i] = diff
+    
     return tdoa
 
 
