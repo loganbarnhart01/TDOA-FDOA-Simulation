17ef47132f90bfc2b599282cbceac1282ccfc49a
Elsa Heidrich <ejheidrich@gmail.com>
2024-06-25 14:19:13 -0600

    working on error ellipses

--- tdoa/solver_tester.py
+++ tdoa/solver_tester.py
@@ -4,8 +4,8 @@ from signal_generator import Emitter, Receiver
 from solver import estimate_emitter
 
 def main():
-    flag = "fdoa"
-    dim = 2
+    flag = "tdoa + fdoa"
+    dim = 3
 
     num_receivers_map = {
         "tdoa + fdoa" : dim + 1,
@@ -32,38 +32,58 @@ def main():
     times_info = [receivers[i].add_time_delay(signals[i], emitter) for i in range(num_receivers)]
     time_signal, times = zip(*times_info)
 
-    times = times + np.random.random(len(times), 1e-6)
-    doppler_freqs = doppler_freqs + np.random.random(len(doppler_freqs), 1e-6)
-    
-    if flag == "tdoa + fdoa":
-        solution = estimate_emitter(receivers, fdoa_data=doppler_freqs, toa_data=times)
-        position = solution[:dim]
-        velocity = solution[dim:]
+    num_trials = 500
+    positions = []
+    velocities = []
+    for i in range(num_trials):
+        times_noise = np.array(times) + np.random.normal(0, 20e-9, len(times))
+        times = times_noise.tolist()
+        doppler_freqs_noise = np.array(doppler_freqs) + np.random.normal(0, 10, len(doppler_freqs))
+        doppler_freqs = doppler_freqs_noise.tolist()
+        if flag == "tdoa + fdoa":
+            solution = estimate_emitter(receivers, fdoa_data=doppler_freqs, toa_data=times)
+            position = solution[:dim]
+            velocity = solution[dim:]
+            positions.append(position)
+            velocities.append(velocity)
 
-    if flag == "tdoa":
-        solution = estimate_emitter(receivers, toa_data=times)
-        position = solution
 
-    if flag == "fdoa no velocity":
-        solution = estimate_emitter(receivers, fdoa_data=doppler_freqs)
-        position = solution[:dim]
-        velocity = solution[dim:]
+        if flag == "tdoa":
+            solution = estimate_emitter(receivers, toa_data=times)
+            position = solution
+            positions.append(position)
 
-    if flag == "fdoa":
-        solution = estimate_emitter(receivers, fdoa_data=doppler_freqs, emitter_velocity=emitter_vel)
-        position = solution
+        if flag == "fdoa no velocity":
+            solution = estimate_emitter(receivers, fdoa_data=doppler_freqs)
+            position = solution[:dim]
+            velocity = solution[dim:]
 
+        if flag == "fdoa":
+            solution = estimate_emitter(receivers, fdoa_data=doppler_freqs, emitter_velocity=emitter_vel)
+            position = solution
+
+    errors_pos = [np.linalg.norm(pos - emitter_pos) for pos in positions]
+    errors_vel = [np.linalg.norm(vel - emitter_vel) for vel in velocities]
+    print(f'Mean error: {np.mean(errors_pos)}')
+    print(f'Std error: {np.std(errors_pos)}')
+    print(f"median error: {np.median(errors_pos)}")
     print(f"True Emitter Position: {emitter_pos}")
-    print(f"Estimated Emitter Position: {position}")
+    
+    # print(f"Estimated Emitter Position: {position}")
+    # print(f"Error in Position: {np.linalg.norm(emitter_pos - position)}")
     
     if flag == "tdoa + fdoa" or flag == "fdoa no velocity":
-        print(f"True Emitter Velocity: {emitter_vel}")
-        print(f"Estimated Emitter Velocity: {velocity}")
 
-    print(f"Error in Position: {np.linalg.norm(emitter_pos - position)}")
+        print(f"True Emitter Velocity: {emitter_vel}")
+        print(f'Mean velocity: {np.mean(velocities)}')
+        print(f'Std velocity: {np.std(velocities)}')
+        # print(f"Estimated Emitter Velocity: {velocity}")
     
     if flag == "tdoa + fdoa" or flag == "fdoa no velocity":
-        print(f"Error in Velocity: {np.linalg.norm(emitter_vel - velocity)}")
+        print(f"Mean Error in Velocity: {np.mean(errors_vel)}")
+        print(f"Median error: {np.median(errors_vel)}")
+        print(f"Std Error in Velocity: {np.std(errors_vel)}")
+
 
 
 if __name__ == "__main__":
