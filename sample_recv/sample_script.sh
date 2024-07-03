9904433375cb38b6d86ea10cb3c43303c7480da8
Elizabeth Hong <ehong@intern1.localdomain>
2024-07-02 18:46:08 -0600

    demod + database

--- sample_recv/sample_script.sh
+++ sample_recv/sample_script.sh
@@ -7,4 +7,4 @@ if [ $? -ne 0 ]; then
 fi
 
 
-bladeRF-cli -e 'set samplerate rx1 2MHz; set frequency rx1 1090MHz; rx config file=rx_20s.sc16q11 format=bin n=0; rx start; rx; rx wait 20s'
+bladeRF-cli -e 'set samplerate rx1 2MHz; set frequency rx1 1090MHz; rx config file=rx_20s.sc16q11 format=bin n=0; rx start; rx; rx wait 40s'
