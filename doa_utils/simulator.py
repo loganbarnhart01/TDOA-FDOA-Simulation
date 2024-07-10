75a373f07170db1e8fdd6341fad60ed443943046
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-07-10 15:27:25 -0600

    tying sim to frontend

--- doa_utils/simulator.py
+++ doa_utils/simulator.py
@@ -5,14 +5,14 @@ import numpy as np
 import random
 import pymap3d as pm
 
-from signal_generator import Emitter, Receiver
-from caf import convolution_caf, fft_caf
-from solver import estimate_emitter
+from .signal_generator import Emitter, Receiver
+from .caf import convolution_caf, fft_caf
+from .solver import estimate_emitter
 
 def simulate_doa(emitter_position: np.ndarray,
                  emitter_velocity: np.ndarray,  
                  receiver_positions: List[np.ndarray], 
-                 message: Optional[str], 
+                 message: Optional[str]=None, 
                  emitter_freq: Optional[int] = 1090e6, 
                  sampling_rate: Optional[int] = 21.80e6,
                  bit_duration: Optional[float] = 1e-6, 
@@ -28,11 +28,11 @@ def simulate_doa(emitter_position: np.ndarray,
         receiver_positions = [pm.geodetic2enu(*pos, lat0, lon0, h0) for pos in receiver_positions]
         
     if message is None:
-        message = ''.join([random.choice('01') for _ in range(10000)])
-    
-    emitter = Emitter(emitter_freq, emitter_position, emitter_velocity)
-    receivers = [Receiver(sampling_rate, bit_duration, pos) for pos in receiver_positions]
-   
+        message = ''.join([random.choice('01') for _ in range(1000)])
+
+    emitter = Emitter(emitter_freq, np.array(emitter_position), np.array(emitter_velocity))
+    receivers = [Receiver(sampling_rate, bit_duration, np.array(pos)) for pos in receiver_positions]
+
     symbols = emitter.generate_signal(message)
     signals = []
 
@@ -46,29 +46,29 @@ def simulate_doa(emitter_position: np.ndarray,
     conv_tdoa_values = [0]
 
     for s in signals[1:]:
-        _, tshift, fshift, _, _ = fft_caf(signals[0], s, 11)
+        _, tshift, fshift, _, _ = fft_caf(signals[0], s, 300)
         fft_fdoa_values.append(fshift * receiver.sample_rate)
         fft_tdoa_values.append(tshift / receiver.sample_rate)
-        _, tshift, fshift, _, _ = convolution_caf(signals[0], s, 11)
-        conv_fdoa_values.append(fshift * receiver.sample_rate)
-        conv_tdoa_values.append(tshift / receiver.sample_rate)
+        # _, tshift, fshift, _, _ = convolution_caf(signals[0], s, 11)
+        # conv_fdoa_values.append(fshift * receiver.sample_rate)
+        # conv_tdoa_values.append(tshift / receiver.sample_rate)
 
     fft_est_emitter = estimate_emitter(receivers, fft_fdoa_values, fft_tdoa_values)
-    conv_est_emitter = estimate_emitter(receivers, conv_fdoa_values, conv_tdoa_values)
+    # conv_est_emitter = estimate_emitter(receivers, conv_fdoa_values, conv_tdoa_values)
 
-    fft_pos = fft_est_emitter[:3]
-    fft_vel = fft_est_emitter[3:]
-    conv_pos = conv_est_emitter[:3]
-    conv_vel = conv_est_emitter[3:]
+    fft_pos = -fft_est_emitter[:3]
+    fft_vel = -fft_est_emitter[3:]
+    # conv_pos = conv_est_emitter[:3]
+    # conv_vel = conv_est_emitter[3:]
 
     fft_pos_error = np.linalg.norm(fft_pos - emitter_position)
     fft_vel_error = np.linalg.norm(fft_vel - emitter_velocity)
-    conv_pos_error = np.linalg.norm(conv_pos - emitter_position)
-    conv_vel_error = np.linalg.norm(conv_vel - emitter_velocity)
+    # conv_pos_error = np.linalg.norm(conv_pos - emitter_position)
+    # conv_vel_error = np.linalg.norm(conv_vel - emitter_velocity)
 
     if not cartesian:
         fft_pos = pm.enu2geodetic(*fft_pos, lat0, lon0, h0)
-        conv_pos = pm.enu2geodetic(*conv_pos, lat0, lon0, h0)
+        # conv_pos = pm.enu2geodetic(*conv_pos, lat0, lon0, h0)
         # still assuming we're good with enu velocity. 
 
-    return fft_pos, fft_vel, conv_pos, conv_vel
+    return np.array(fft_pos), np.array(fft_vel) #fft_pos_error, fft_vel_error #, conv_pos, conv_vel, fft_pos_error, fft_vel_error, conv_pos_error, conv_vel_error
